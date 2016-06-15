f5369a3a2892ed270fb535b6aa74cf238b402d13
Wesley Hall <wesleyahall@gmail.com>
2016-06-15 12:37:46 -0500

    adds FooterNav component

---
+++ www/components/FooterNav/FooterNav.js
@@ -0,0 +1,22 @@
+import './FooterNav.styl'
+import React from 'react'
+import { Link } from 'react-router'
+
+export default (props) => {
+  return (
+    <footer>
+      <nav className='footer-nav'>
+        <Link to='/' className='footer-nav__link' activeClassName='footer-nav__link--active'>
+          <div className='footer-nav__link__item footer-nav__link__item--directory'>
+            Directory
+          </div>
+        </Link>
+        <Link to='/appointment' className='footer-nav__link' activeClassName='footer-nav__link--active'>
+          <div className='footer-nav__link__item footer-nav__link__item--appointment'>
+            CCI Appt.
+          </div>
+        </Link>
+      </nav>
+    </footer>
+  )
+}
