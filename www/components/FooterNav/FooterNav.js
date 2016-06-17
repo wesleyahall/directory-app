5701ce5e24278e52b1a40209cacc13f15951e1e5
Wesley Hall <wesleyahall@gmail.com>
2016-06-17 12:00:18 -0500

    getting rid of overscroll on mobile

--- www/components/FooterNav/FooterNav.js
+++ www/components/FooterNav/FooterNav.js
@@ -8,13 +8,11 @@ export default (props) => {
       <nav className='footer-nav'>
         <Link to='/' className='footer-nav__link' activeClassName='footer-nav__link--active'>
           <div className='footer-nav__link__item footer-nav__link__item--directory'>
-            <i className='fa fa-book' aria-hidden='true'></i>
             Directory
           </div>
         </Link>
         <Link to='/appointment' className='footer-nav__link' activeClassName='footer-nav__link--active'>
           <div className='footer-nav__link__item footer-nav__link__item--appointment'>
-            <i className='fa fa-calendar-plus-o' aria-hidden='true'></i>
             Appointments
           </div>
         </Link>
